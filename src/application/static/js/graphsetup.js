8b0db738f650bec20a4666b40be67f3ac250d57b
Chloe Fan <herchloeness@gmail.com>
2012-02-22 15:02:19 -0500

    Adding auto refresh to column graph

--- src/application/static/js/graphsetup.js
+++ src/application/static/js/graphsetup.js
@@ -43,23 +43,6 @@ var logInterval = 5;
 var lowStepRange = (lowRangeOfWalkingSpeed * FEET_PER_MILE)/MINS_PER_HOUR/strideLength * logInterval;
 var highStepRange = (highRangeOfWalkingSpeed * FEET_PER_MILE)/MINS_PER_HOUR/strideLength * logInterval;
 var maxStepRange = (runningSpeed * FEET_PER_MILE)/MINS_PER_HOUR/strideLength * logInterval;
-console.log(lowStepRange, highStepRange, maxStepRange);
-
-function setColorDemo(home,out) {
-	var h;
-	var s = 0.88;
-	var b = 0.88;
-	if (home) {
-		h = Math.random() * 0.16; // red-yellow, hue 0-0.16
-	}
-	if (!home && out) {
-		h = Math.random() * 0.39 + 0.19; // green-blue, hue 0.19-0.58
-	}
-	if (!home && !out) {
-		h = Math.random() * 0.33 + 0.61; // purple-pink, hue 0.61-0.94
-	}
-	return "hsb("+h+","+s+","+b+")";
-}
 
 function setColor(steps) {
 	var h = calculateHueFromSteps(steps, lowStepRange, highStepRange, maxStepRange);
